40d516affca16885a0d3c0f6884c1d65fa252027
lebbis <44391814+lebbis@users.noreply.github.com>
2021-02-25 15:39:32 +0200

    Backup

---
+++ Digicenter_hololens_v2/Assets/DestroyByPosition.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByPosition : MonoBehaviour
+{
+    public float destroyPositionY = -2;
+    public float destroyPositionZ = -2;
+    void Update()
+    {
+        if (transform.position.y < destroyPositionY ||
+            transform.position.z < destroyPositionZ)
+        {
+            Destroy(this.gameObject); //Destroy gameobject if fallen 10 meters below ground level
+        }
+    }
+
+    //private void OnCollisionEnter(Collision collision)
+    //{
+    //    if (collision.gameObject.CompareTag("Floor"))
+    //    {
+    //        Destroy(this.gameObject);
+    //    }
+    //}
+}
---
+++ Digicenter_hololens_v2/Assets/Scripts/Damage.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damage : MonoBehaviour
+{
+    public float damage = 100;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Target1"))
+        {
+            collision.gameObject.SendMessage("OnDamage", damage);
+        }
+    }
+}
